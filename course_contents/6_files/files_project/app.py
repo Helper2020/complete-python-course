d347a1b0f22fe2562dc8123195abc7da3663e9ee
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2020-03-01 16:55:56 -0500

    Added some notes as comments.

--- course_contents/6_files/files_project/app.py
+++ course_contents/6_files/files_project/app.py
@@ -1,4 +1,6 @@
+# file pointer
 my_file = open('data.txt', 'r')
+# reads entire content as single string
 file_content = my_file.read()
 
 my_file.close()
@@ -6,7 +8,8 @@ my_file.close()
 print(file_content)
 
 user_name = input('Enter your name: ')
-
+# 'w' erases the content of the file
+# We want to open the file for the smallest amount of time
 my_file_writing = open('data.txt', 'w')
 my_file_writing.write(user_name)
 
